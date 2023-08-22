65179927f4cc6735e368a744340e091066166312
aid3nlee <ajlee5@madison.k12.wi.us>
2023-08-22 12:24:20 -0500

    Nonifications Final

--- Login auth app/Main/WestHighApp.swift
+++ Login auth app/Main/WestHighApp.swift
@@ -9,10 +9,59 @@ import Firebase
 
 @main // main function, runs app
 struct WestHighApp: App {
-    
     init() {
+        let centerl = UNUserNotificationCenter.current()
+        centerl.requestAuthorization(options: [.alert, .sound, .badge]) {result, error in
+            if let error = error{
+                print(error)
+            }
+        }
+        
+        struct todaysdate {
+            
+            var daysToAdd: Int
+            
+            func isWeekday() -> Bool {
+                let dayFormatter = DateFormatter()
+                let todaysDate = Date()
+                dayFormatter.dateFormat = "EEEE"
+                var dateComponent = DateComponents()
+                dateComponent.day = daysToAdd
+                let day = dayFormatter.string(from: Calendar.current.date(byAdding: dateComponent, to: todaysDate)!)
+                var returnValue = true
+                if day == "Saturday" || day == "Sunday" {
+                    returnValue = false
+                }
+                return returnValue
+            }
+        }
+        
+    
+        if todaysdate(daysToAdd: 0).isWeekday(){
+            
+            let center = UNUserNotificationCenter.current()
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Good Morning Regents!"
+            content.body = "Check the app for your daily announcements!"
+            
+            var dateInfo = DateComponents()
+            dateInfo.hour = 8
+            dateInfo.minute = 15
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateInfo, repeats: true)
+            
+            //creating request
+            let request = UNNotificationRequest(identifier: "Identifier", content: content, trigger: trigger)
+            
+            center.add(request){error in
+                if let error = error {
+                    print(error)
+                }
+            }
+        }
         FirebaseApp.configure()
     }
+    
     var body: some Scene {
         WindowGroup {
             AuthView() // starts at the authentication screen
