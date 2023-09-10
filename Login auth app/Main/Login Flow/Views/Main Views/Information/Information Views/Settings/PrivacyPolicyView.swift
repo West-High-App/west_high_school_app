49ea5a80cb2ab947820d8bd56d95331ddefe5ecd
August Andersen <augustelholm@gmail.com>
2023-09-11 00:56:37 +0200

    updated privacy policy

--- Login auth app/Main/Login Flow/Views/Main Views/Information/Information Views/Settings/PrivacyPolicyView.swift
+++ Login auth app/Main/Login Flow/Views/Main Views/Information/Information Views/Settings/PrivacyPolicyView.swift
@@ -27,15 +27,15 @@ struct PrivacyPolicyView: View {
                     }
 
                     VStack(alignment: .leading){
-                        Text("West High built the West High App app as a Free app. This SERVICE is provided by West High at no cost and is intended for use as is.\n")
-                        Text("This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.\n")
+                        Text("West High built the West High App app as a free app. This SERVICE is provided by West High at no cost and is intended for use as is.\n")
+                        Text("This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decides to use our Service.\n")
                         Text("If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.\n")
                         Text("The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at West High App unless otherwise defined in this Privacy Policy.\n")
                         HStack{
                             Text("**Information Collection and Use**")
                             Spacer()
                         }
-                        Text("For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to email and name. The information that we request will be retained by us and used as described in this privacy policy.")
+                        Text("For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name and email. The information that we request will be retained by us and used as described in this privacy policy.")
                         Text("The app does use third-party services that may collect information used to identify you.")
                         Text("Link to the privacy policy of third-party service providers used by the app\n")
                     }
@@ -56,7 +56,7 @@ struct PrivacyPolicyView: View {
                         Text("To perform Service-related services")
                         Text("We want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.\n")
                         Text("**Security**")
-                        Text("We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.\n")
+                        Text("We value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is fully secure and reliable, therefore we cannot guarantee its absolute security.\n")
                     }
                     VStack(alignment: .leading){
                         Text("**Links to Other Sites**")
@@ -65,9 +65,9 @@ struct PrivacyPolicyView: View {
                         Text("These Services do not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13 years of age. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do the necessary actions.\n")
                         Text("**Changes to This Privacy Policy**")
                         Text("We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.")
-                        Text("This policy is effective as of 2023-08-30\n")
+                        Text("This policy is effective as of 2023-09-11\n")
                         Text("**Contact Us**")
-                        Text("If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us at westhighapp@gmail.com.\n")
+                        Text("If you have any questions or suggestions about our Privacy Policy, contact us at westhighapp@gmail.com.\n")
                     }
                     
                     
@@ -75,6 +75,7 @@ struct PrivacyPolicyView: View {
                     
                     
                 }
+                .padding(.vertical, -50)
                 .padding(.horizontal,10)
             
             
