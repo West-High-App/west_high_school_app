4bd14aaeffa2f1f280e5f5552a504351503991de
Neo-785 <augustelholm@gmail.com>
2023-09-11 23:31:27 +0200

    loading, homeview, spotlight images, announcements, filters

--- Login auth app/Main/Login Flow/Views/Main Views/Announcements/AnnouncementsDetailView.swift
+++ Login auth app/Main/Login Flow/Views/Main Views/Announcements/AnnouncementsDetailView.swift
@@ -3,32 +3,27 @@ import SwiftUI
 struct AnnouncementsDetailView: View {
     var currentnews: Newstab
     var body: some View {
-        ZStack {
             ScrollView{
                 VStack{
-                    Image(currentnews.newsimagename)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
-                        .cornerRadius(15)
-                    //.padding(.leading,10)
                     Text(currentnews.title)
                     //.padding(.leading,10)
                         .padding(.horizontal)
                         .multilineTextAlignment(.center)
                         .foregroundColor(Color.black)
-                        .font(.system(size: 40, weight: .semibold, design: .rounded))
+                        .font(.system(size: 36, weight: .semibold, design: .rounded))
+                        .lineLimit(2)
                     HStack{
                         Text(currentnews.publisheddate)
                         //.padding(.leading,13)
                             .foregroundColor(Color.gray)
                             .font(.system(size: 26, weight: .semibold, design: .rounded))
+                            .lineLimit(1)
                     }
 
                     Text(currentnews.description)
                         .multilineTextAlignment(.leading)
                         .foregroundColor(Color.black)
-                        .font(.system(size: 24, weight: .regular, design: .rounded))
+                        .font(.system(size: 17, weight: .regular, design: .rounded))
                         .padding(.horizontal, 25)
                         .padding(.vertical)
                         .background(Rectangle()
@@ -37,17 +32,16 @@ struct AnnouncementsDetailView: View {
                             .shadow(radius: 5, x: 3, y: 3)
                             .foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 0.94)))
                         .padding(.vertical)
-
+                    Spacer()
                 }
                 .padding(.top)
             }
-        }
     }
 }
 
 struct AnnouncementsDetailView_Previews: PreviewProvider {
     var newsDataManager = Newslist()
     static var previews: some View {
-        AnnouncementsDetailView(currentnews: Newstab(documentID: "testID", title: "Test", publisheddate: "Jan 5, 2023", description: "Description", newsimagename: "West Regents Logo"))
+        AnnouncementsDetailView(currentnews: Newstab(documentID: "testID", title: "This is my title yessir it pretty long", publisheddate: "Jan 5, 2023", description: "Description mouhwe gou weoguh weoug wneouwehg oweun woeung oweugh woe ngouwenf owuend woe ouwengouw efnoweu dnouweng owuehg oweunf ouwednouwe owuenc owueng owuen ouwn ouwenoguwe ouwendouwe gouwen owueh uowecnoweun wouenc weoug hwouenwo uenf.", newsimagename: "West Regents Logo"))
     }
 }
