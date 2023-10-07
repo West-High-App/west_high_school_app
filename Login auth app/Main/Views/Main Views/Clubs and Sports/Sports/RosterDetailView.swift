492692c4d3495eade96f1a3fff5c402c84389c74
August Andersen <augustelholm@gmail.com>
2023-10-07 16:40:46 +0200

    updates roster detail view

--- Login auth app/Main/Views/Main Views/Clubs and Sports/Sports/RosterDetailView.swift
+++ Login auth app/Main/Views/Main Views/Clubs and Sports/Sports/RosterDetailView.swift
@@ -12,34 +12,56 @@ struct ZoomableImageView: View {
     var body: some View {
         let magnifiedScale = max(scale * magnifyBy, 1.0)
         
-        return Image(uiImage: image)
-            .resizable()
-            .scaledToFit()
-            .scaleEffect(magnifiedScale)
-            .offset(
-                CGSize(
-                    width: offset.width + dragOffset.width,
-                    height: offset.height + dragOffset.height
+        return
+        ZStack {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(magnifiedScale)
+                .offset(
+                    CGSize(
+                        width: offset.width + dragOffset.width,
+                        height: offset.height + dragOffset.height
+                    )
                 )
-            )
-            .gesture(
-                MagnificationGesture()
-                    .updating($magnifyBy) { value, state, _ in
-                        state = value
-                    }
-                    .onEnded { value in
-                        scale *= value
-                    }
-            )
-            .gesture(
-                DragGesture()
-                    .updating($dragOffset) { value, state, _ in
-                        state = value.translation
-                    }
-                    .onEnded { value in
-                        offset.width += value.translation.width
-                        offset.height += value.translation.height
-                    }
-            )
+                .gesture(
+                    MagnificationGesture()
+                        .updating($magnifyBy) { value, state, _ in
+                            state = value
+                        }
+                        .onEnded { value in
+                            scale *= value
+                        }
+                )
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, state, _ in
+                            state = value.translation
+                        }
+                        .onEnded { value in
+                            offset.width += value.translation.width
+                            offset.height += value.translation.height
+                        }
+                )
+            
+            VStack {
+                Spacer()
+                Button("Reset") {
+                    scale = 1
+                    offset = .zero
+                }.foregroundColor(.blue)
+                    .fontWeight(.semibold)
+                    .padding(10)
+                    .cornerRadius(15.0)
+                    .font(.system(size: 17, weight: .semibold, design: .rounded))
+                    .background(Rectangle()
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    )
+                .padding()
+                    .padding(.bottom, 20)
+                
+            }
+        }
     }
 }
