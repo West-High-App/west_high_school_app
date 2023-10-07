49df7862e633c8ef2f8078ef699391078a7626ed
aid3nlee <ajlee5@madison.k12.wi.us>
2023-10-07 14:10:13 -0500

    roster image view

---
+++ Login auth app/Main/Views/Main Views/Clubs and Sports/Sports/Another Info Thing.swift
@@ -0,0 +1,26 @@
+//
+//  Another Info Thing.swift
+//  West High
+//
+//  Created by Aiden Lee on 10/7/23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
--- Login auth app/Main/Views/Main Views/Clubs and Sports/Sports/RosterDetailView.swift
+++ Login auth app/Main/Views/Main Views/Clubs and Sports/Sports/RosterDetailView.swift
@@ -1,67 +1,158 @@
 import SwiftUI
 
 struct ZoomableImageView: View {
-    @GestureState private var magnifyBy: CGFloat = 1.0
-    @GestureState private var dragOffset: CGSize = .zero
     
-    @State private var scale: CGFloat = 1.0
-    @State private var offset: CGSize = .zero
-    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = CGSize.zero
     let image: UIImage
+    func resetImageState() {
+   
+        return withAnimation(.spring()){
+            imageScale = 1
+            imageOffset = .zero
+         
+        }
+    }
+    
     
     var body: some View {
-        let magnifiedScale = max(scale * magnifyBy, 1.0)
-        
-        return
-        ZStack {
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFit()
-                .scaleEffect(magnifiedScale)
-                .offset(
-                    CGSize(
-                        width: offset.width + dragOffset.width,
-                        height: offset.height + dragOffset.height
-                    )
-                )
-                .gesture(
-                    MagnificationGesture()
-                        .updating($magnifyBy) { value, state, _ in
-                            state = value
-                        }
-                        .onEnded { value in
-                            scale *= value
-                        }
-                )
-                .gesture(
-                    DragGesture()
-                        .updating($dragOffset) { value, state, _ in
-                            state = value.translation
-                        }
-                        .onEnded { value in
-                            offset.width += value.translation.width
-                            offset.height += value.translation.height
-                        }
-                )
             
-            VStack {
-                Spacer()
-                Button("Reset") {
-                    scale = 1
-                    offset = .zero
-                }.foregroundColor(.blue)
-                    .fontWeight(.semibold)
-                    .padding(10)
-                    .cornerRadius(15.0)
-                    .font(.system(size: 17, weight: .semibold, design: .rounded))
-                    .background(Rectangle()
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+            ZStack{
+                Color.clear
+                Image(uiImage: image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x:2, y: 2)
+                    .opacity(isAnimating ? 1: 0)
+                    .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()){
+                                imageScale = 5
+                                
+                            }
+                        }else{
+                            resetImageState()
+                            
+                        }
+                    }
+                    .gesture(DragGesture()
+                        .onChanged{ value in
+                            withAnimation(.linear(duration: 1)){
+                                if imageScale <= 1  {
+                                    imageOffset = value.translation
+                                }else{
+                                    imageOffset = value.translation
+
+                                }
+                                
+                               
+                            }
+                        }
+                        .onEnded{ value in
+                            if imageScale <= 1 {
+                                resetImageState()
+                            }
+                            
+                          
+                        }
                     )
-                .padding()
-                    .padding(.bottom, 20)
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged{ value in
+                                withAnimation(.linear(duration: 1)){
+                                    if imageScale >= 1 && imageScale <= 5 {
+                                        imageScale = value
+                                    } else if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            .onEnded{ _ in
+                                
+                                if imageScale > 5 {
+                                    imageScale = 5
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                                      
+                    
+                    )
                 
+            }
+            .padding(.bottom, 70)
+            .onAppear {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            }
+
+            .overlay(
+                Group{
+                    HStack{
+                        // SCALE DOWN
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                           ControlImageView(icon: "minus.magnifyingglass")
+                        }
+                        
+                        // RESET
+                        Button{
+                            resetImageState()
+                        } label: {
+                           ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        // SCALE UP
+                        Button{
+                            withAnimation(.spring()){
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                        } label: {
+                           ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                        
+                    }//: HSTACK
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimating ? 1 : 0)
+                    
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            )
+            
+            
+            
+        
+        .navigationViewStyle(.stack)
+        .onTapGesture(count: 3) {
+            if(imageScale > 1){
+                resetImageState()
             }
         }
     }
 }
+
