3925cff6e19400f812dd9342b12d6ee746edfe9e
August Andersen <augustelholm@gmail.com>
2023-11-06 22:13:07 +0100

    club event admin rework

--- Login auth app/Main/Views/Main Views/Clubs and Sports/Clubs/ClubEventAdminView.swift
+++ Login auth app/Main/Views/Main Views/Clubs and Sports/Clubs/ClubEventAdminView.swift
@@ -19,7 +19,7 @@ struct ClubsEventsAdminView: View {
     @State var eventToSave: clubEvent?
     @State var editingeventslist: [clubEvent] = []
     @State var screen = ScreenSize()
-    
+        
     @State var selectedevent = 0
     @State var selectedtime = 0
     
@@ -46,6 +46,8 @@ struct ClubsEventsAdminView: View {
     @State var subtitle = ""
     @State var date = ""
     
+    @State var selectedclubevent: clubEvent?
+        
     var body: some View {
         
         VStack {
@@ -126,8 +128,8 @@ struct ClubsEventsAdminView: View {
         }.navigationTitle("Edit Club Events")
             .alert(isPresented: $isConfirmingDeleteEvent) {
                 Alert(
-                    title: Text("You Are Deleting Public Data"),
-                    message: Text("Are you sure you want to delete the event '\(temptitle)'? \nOnce deleted, the data can no longer be retrieved and will disappear from the app.\nThis action cannot be undone."),
+                    title: Text("Delete Event"),
+                    message: Text("This action cannot be undone."),
                     primaryButton: .destructive(Text("Delete")) {
                         if let eventToDelete = eventToDelete {
                             dataManager.deleteClubEvent(forClub: "\(currentclub.clubname)", clubEvent: eventToDelete)
@@ -136,116 +138,181 @@ struct ClubsEventsAdminView: View {
                     secondaryButton: .cancel(Text("Cancel"))
                 )
             }
+        
             .sheet(isPresented: $isPresetingAddEvent) {
-                VStack {
-                    HStack {
-                        Button("Cancel") {
-                            isPresetingAddEvent = false
-                        }.padding()
-                        Spacer()
-                    }
-                    Form {
-                        Section(header: Text("Clubs Event details")) {
-                            if admin {
-                                //bob
-                                TextField("Title", text: $title)
-                                DatePicker("Pick a time", selection: $selectedTime, displayedComponents: .hourAndMinute)
-                                    .labelsHidden()
-                                    .datePickerStyle(WheelDatePickerStyle())
-                                
-                            } else {
-                                Picker("Event Type", selection: $selectedevent) {
-                                    Text("Meeting")
-                                        .tag(0)
-                                    Text("Mandatory Meeting")
-                                        .tag(1)
-                                    Text("Competition")
-                                        .tag(2)
-                                    Text("Special Event")
-                                        .tag(3)
-                                }
-                                DatePicker("Pick a time", selection: $selectedTime, displayedComponents: .hourAndMinute)
-                                    .labelsHidden()
-                                    .datePickerStyle(WheelDatePickerStyle())
-                            }
-                            
-                            Picker("Month", selection: $selectedMonthIndex) {
-                                ForEach(0..<months.count, id: \.self) { index in
-                                    Text(months[index]).tag(index)
-                                }
-                            }
-                            
-                            Picker("Day", selection: $selectedDayIndex) {
-                                ForEach(0..<days.count, id: \.self) { index in
-                                    Text("\(days[index])").tag(index)
-                                }
-                            }
-                            
-                            Picker("Year", selection: $selectedYearIndex) {
-                                ForEach(0..<years.count, id: \.self) { index in
-                                    Text("\(years[index])").tag(index)
-                                }
-                            }
-                        }
-                        Button {
-                            if admin {
-                                eventToSave = clubEvent(
-                                    documentID: "NAN",
-                                    title: title,
-                                    subtitle: formattedTime,
-                                    month: months[selectedMonthIndex],
-                                    day: "\(days[selectedDayIndex])",
-                                    year: years[selectedYearIndex],
-                                    publisheddate: "\(months[selectedMonthIndex])   \(days[selectedDayIndex]),\(eventyear)")
-                            } else {
-                                eventToSave = clubEvent(
-                                    documentID: "NAN",
-                                    title: typelist[selectedevent],
-                                    subtitle: formattedTime,
-                                    month: months[selectedMonthIndex],
-                                    day: "\(days[selectedDayIndex])",
-                                    year: years[selectedYearIndex],
-                                    publisheddate: "\(months[selectedMonthIndex])   \(days[selectedDayIndex]),\(eventyear)")
-                            }
-                            if let eventToSave = eventToSave {
-                                print("Event to save")
-                                print(eventToSave)
-                                
-                                editingeventslist.append(eventToSave)
-                                
-                                dataManager.createClubEvent(forClub: "\(currentclub.clubname)", clubEvent: eventToSave)
-                                isPresetingAddEvent = false
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                    isPresentingConfirmEvent = true // here?
-                                }
-                            }
-                        } label: {
-                            Text("Publish New Club Event")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .padding(10)
-                                .cornerRadius(15.0)
-                                .frame(width: screen.screenWidth-60)
-                                .font(.system(size: 17, weight: .semibold, design: .rounded))
-                                .background(Rectangle()
-                                    .foregroundColor(.red)
-                                    .cornerRadius(10)
-                                )
-                        }
-                    }
-                    .onAppear{
-                        for i in 0...2 {
-                            years.append(String(Int(year) + Int(i)))
-                        }
-                        let currentYear = calendar.component(.year, from: Date())
-                        eventyear = String(currentYear)
-                    }
-                }
-                .navigationBarTitle("Edit Club Events")
-                
+                ClubEventAdminDetailView(dataManager: dataManager, currentclub: currentclub, admin: admin)
             }
+            .sheet(item: $selectedclubevent) { event in
+                ClubEventAdminDetailView(dataManager: dataManager, editingEvent: event, currentclub: currentclub, admin: admin)
+            }
+            
     }
 }
+
+
+struct ClubEventAdminDetailView: View {
+   @Environment(\.presentationMode) var presentationMode
+   @ObservedObject var dataManager: clubEventManager
+   @State private var eventname = ""
+   var editingEvent: clubEvent?
+    var currentclub: club
+    
+    @State var isConfirmingDeleteEvent = false
+    @State var isPresetingAddEvent = false
+    @State var isPresentingConfirmEvent = false
+    @State var eventToDelete: clubEvent?
+    @State var eventToSave: clubEvent?
+    @State var editingeventslist: [clubEvent] = []
+    @State var screen = ScreenSize()
+    
+    @State var selectedevent = 0
+    @State var selectedtime = 0
+    
+    var typelist = ["Meeting", "Mandatory Meeting", "Competition", "Special Event"]
+    
+    @State private var selectedTime = Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: Date()) ?? Date()
+    var formattedTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        return dateFormatter.string(from: selectedTime)
+    }
+    
+    @State private var eventyear = ""
+    let calendar = Calendar.current
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    let days = Array(1...31)
+    @State var years: [String] = []
+    let year = Calendar.current.component(.year, from: Date())
+    @State private var selectedMonthIndex = Calendar.current.component(.month, from: Date()) - 1
+    @State private var selectedYearIndex = 0
+    @State private var selectedDayIndex = Calendar.current.component(.day, from: Date()) - 1
+    
+    @State var title = ""
+    @State var subtitle = ""
+    @State var date = ""
+    
+    @State var isPresentingAddClub = false
+    
+    var admin: Bool
+      
+   @State private var isConfirmingAddClubEvent = false
+   
+   var body: some View {
+       NavigationView {
+           Form {
+               Section(header: Text("Clubs Event details")) {
+                   if admin {
+                       //bob
+                       TextField("Title", text: $title)
+                       DatePicker("Pick a time", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                           .labelsHidden()
+                           .datePickerStyle(WheelDatePickerStyle())
+                       
+                   } else {
+                       Picker("Event Type", selection: $selectedevent) {
+                           Text("Meeting")
+                               .tag(0)
+                           Text("Mandatory Meeting")
+                               .tag(1)
+                           Text("Competition")
+                               .tag(2)
+                           Text("Special Event")
+                               .tag(3)
+                       }
+                       DatePicker("Pick a time", selection: $selectedTime, displayedComponents: .hourAndMinute)
+                           .labelsHidden()
+                           .datePickerStyle(WheelDatePickerStyle())
+                   }
+                   
+                   Picker("Month", selection: $selectedMonthIndex) {
+                       ForEach(0..<months.count, id: \.self) { index in
+                           Text(months[index]).tag(index)
+                       }
+                   }
+                   
+                   Picker("Day", selection: $selectedDayIndex) {
+                       ForEach(0..<days.count, id: \.self) { index in
+                           Text("\(days[index])").tag(index)
+                       }
+                   }
+                   
+                   Picker("Year", selection: $selectedYearIndex) {
+                       ForEach(0..<years.count, id: \.self) { index in
+                           Text("\(years[index])").tag(index)
+                       }
+                   }
+               }
+               
+               Button {
+                   isConfirmingAddClubEvent = true
+               } label: {
+                   Text("Publish New Club Event")
+                       .foregroundColor(.white)
+                       .fontWeight(.semibold)
+                       .padding(10)
+                       .cornerRadius(15.0)
+                       .frame(width: screen.screenWidth-60)
+                       .font(.system(size: 17, weight: .semibold, design: .rounded))
+                       .background(Rectangle()
+                           .foregroundColor(.blue)
+                           .cornerRadius(10)
+                       )
+               }
+           }
+               
+           .navigationBarTitle("Add Club Event")
+           .navigationBarItems(leading: Button("Cancel") {
+               presentationMode.wrappedValue.dismiss()
+           })
+           .alert(isPresented: $isConfirmingAddClubEvent) {
+               Alert(
+                   title: Text("Publish Club Event"),
+                   message: Text("This action cannot be undone."),
+                   primaryButton: .default(Text("Publish")) {
+                       if admin {
+                           eventToSave = clubEvent(
+                               documentID: "NAN",
+                               title: title,
+                               subtitle: formattedTime,
+                               month: months[selectedMonthIndex],
+                               day: "\(days[selectedDayIndex])",
+                               year: years[selectedYearIndex],
+                               publisheddate: "\(months[selectedMonthIndex])   \(days[selectedDayIndex]),\(eventyear)")
+                       } else {
+                           eventToSave = clubEvent(
+                               documentID: "NAN",
+                               title: typelist[selectedevent],
+                               subtitle: formattedTime,
+                               month: months[selectedMonthIndex],
+                               day: "\(days[selectedDayIndex])",
+                               year: years[selectedYearIndex],
+                               publisheddate: "\(months[selectedMonthIndex])   \(days[selectedDayIndex]),\(eventyear)")
+                       }
+                       if let eventToSave = eventToSave {
+                           print("Event to save")
+                           print(eventToSave)
+                           
+                           editingeventslist.append(eventToSave)
+                           
+                           dataManager.createClubEvent(forClub: "\(currentclub.clubname)", clubEvent: eventToSave)
+                       }
+                       presentationMode.wrappedValue.dismiss()
+                   },
+                   secondaryButton: .cancel()
+               )
+           }
+           .onAppear{
+               for i in 0...2 {
+                   years.append(String(Int(year) + Int(i)))
+               }
+               let currentYear = calendar.component(.year, from: Date())
+               eventyear = String(currentYear)
+           }
+       }
+   }
+}
+
+
 
 struct ClubContentView_Previews: PreviewProvider {
     static var previews: some View {
