92949aa17784f9886116b41e3eecf0ca5edf1fd5
August Andersen <augustelholm@gmail.com>
2024-01-03 07:06:52 +0100

    fixed webviews loading

--- West High App/Main Views/Home/HomeView.swift
+++ West High App/Main Views/Home/HomeView.swift
@@ -105,7 +105,7 @@ struct HomeView: View {
                     ClassesView()
                     SchoolPolicyGuideView()
                }
-               //MARK: HOME
+               
                if !isShutDown {
                     NavigationView{
                          ScrollView(.vertical, showsIndicators: false) {
@@ -434,7 +434,6 @@ struct HomeView: View {
                          hasPermission.updatePermissions()
                     }
                }
-               
                hasAppeared = true
                loadWebViews = false
           }
