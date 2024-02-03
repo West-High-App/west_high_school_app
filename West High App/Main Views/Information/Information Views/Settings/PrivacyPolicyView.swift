215469baacc477a912480debf21aa18db40d10f3
August Andersen <augustelholm@gmail.com>
2024-02-03 13:12:07 +0100

    Privacy Policy updates

--- West High App/Main Views/Information/Information Views/Settings/PrivacyPolicyView.swift
+++ West High App/Main Views/Information/Information Views/Settings/PrivacyPolicyView.swift
@@ -12,7 +12,7 @@ struct PrivacyPolicyView: View {
         ScrollView{
                 VStack{
                     VStack(alignment: .leading){
-                        Text("West High built the West High App app as a free app. This SERVICE is provided by West High at no cost and is intended for use as is.\n")
+                        Text("West High built the West High App app as a free app. The West High App (\"the App\" or the \"Service\") is provided by West High at no cost and is intended for use as is.\n")
                         Text("This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decides to use our Service.\n")
                         Text("If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.\n")
                         Text("The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at West High App unless otherwise defined in this Privacy Policy.\n")
@@ -21,17 +21,17 @@ struct PrivacyPolicyView: View {
                             Spacer()
                         }
                         Text("For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to your name and email. The information that we request will be retained by us and used as described in this privacy policy.")
-                        Text("The app does use third-party services that may collect information used to identify you.")
-                        Text("Link to the privacy policy of third-party service providers used by the app\n")
+                        Text("The App uses third-party services that may collect information used to identify you.")
+                        Text("Privacy Policies of third-party service providers used by the App are provided below:\n")
                     }
                     VStack(alignment: .leading){
                         Text("[Google Analytics for Firebase](https://www.google.com/analytics/terms/)\n")
                         Text("[Firebase Crashlytics](https://firebase.google.com/terms/crashlytics)\n")
                         Text("**Log Data**")
-                        Text("We want to inform you that whenever you use our Service, in a case of an error in the app we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing our Service, the time and date of your use of the Service, and other statistics.\n")
+                        Text("We want to inform you that whenever you use our Service, in a case of an error in the App we collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the App when utilizing our Service, the time and date of your use of the Service, and other statistics.\n")
                         Text("**Cookies**")
                         Text("Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.")
-                        Text("This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.\n")
+                        Text("This Service does not use these “cookies” explicitly. However, the App may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.\n")
                     }
                     VStack(alignment: .leading){
                         Text("**Service Providers**")
